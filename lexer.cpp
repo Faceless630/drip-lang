0d93fbad1a427749e5ddcc6c91de2c1fcd7225a4
Faceless <63298430+KEN8111git@users.noreply.github.com>
2020-11-21 22:20:56 +0800

    Update lexer.cpp

--- lexer.cpp
+++ lexer.cpp
@@ -4,31 +4,72 @@
 
 std::string identifier_decision, currentchar;
 
+inline void reset() { identifier_decision = ""; }
+
 int search_token(Identifiers &identifier)
 {
-  while (currentchar != " ")
-  {
-    if (currentchar == "+")
-      return ADD_OPERATOR;
-    else if (currentchar == "-")
-      return SUB_OPERATOR;
-    else if (currentchar == "*")
-      return SUB_OPERATOR;
-    else if (currentchar == "/")
-      return DIV_OPERATOR;
-    else if (currentchar == "%")
-      return PERCENT_OPERATOR;
-    else if (currentchar == "\n")
-      return NEWLINE_OPERATOR;
-    else if (currentchar == "(")
-      return LP;
-    else if (currentchar == ")")
-      return RP;
-    else if (currentchar == "{")
-      return LC;
-    else if (currentchar == "}")
-      return RC;
 
-    identifier_decision += currentchar;
+  if (currentchar == "+")
+    return ADD_OPERATOR;
+  else if (currentchar == "-")
+    return SUB_OPERATOR;
+  else if (currentchar == "*")
+    return SUB_OPERATOR;
+  else if (currentchar == "/")
+    return DIV_OPERATOR;
+  else if (currentchar == "%")
+    return PERCENT_OPERATOR;
+  else if (currentchar == "\n")
+    return NEWLINE_OPERATOR;
+  else if (currentchar == "(")
+    return LP;
+  else if (currentchar == ")")
+    return RP;
+  else if (currentchar == "{")
+    return LC;
+  else if (currentchar == "}")
+    return RC;
+
+  identifier_decision += currentchar;
+  if (identifier_decision == "var")
+  {
+    reset();
+    return var_kw;
+  }
+
+  else if (identifier_decision == "if")
+  {
+    reset();
+    return if_kw;
+  }
+
+  else if (identifier_decision == "elif")
+  {
+    reset();
+    return elif_kw;
+  }
+
+  else if (identifier_decision == "else")
+  {
+    reset();
+    return else_kw;
+  }
+
+  else if (identifier_decision == "for")
+  {
+    reset();
+    return for_kw;
+  }
+
+  else if (identifier_decision == "while")
+  {
+    reset();
+    return while_kw;
+  }
+
+  else if (identifier_decision == "func")
+  {
+    reset();
+    return func_kw;
   }
 }
